a515878fe121c14e57a311b88348d5dfe9f1aa94
SaiKumar <saikumar.vodhulu@qentelli.com>
2022-11-07 20:32:27 +0530

    SH

--- Jmeterrun.sh
+++ Jmeterrun.sh
@@ -1,25 +1,24 @@
 #!bin/bash
 
+echo "*************Running Jmeterun.sh file**********"
+
 #git config --global http.sslverify false
 #git clone https://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@github.com/svodhulu/Docker.git
 
-echo "*************Running Jmeterun.sh file**********"
 
-cd ../bin
+cd ..
 
-
-
-curTime= $(date+"%m-%d-%y_%H-%M-%S")
-curTime= $(echo $curTime|sed 's/-//g'|sed 's/_//g')
+#curTime= $(date+"%m-%d-%y_%H-%M-%S")
+#curTime= $(echo $curTime|sed 's/-//g'|sed 's/_//g')
 
 echo "Current time in epochtime $curTime"
 
 
-./jmeter.sh -n -t /opt/apache-jmeter-5.5/bin/Docker/SampleTestScript_Google.jmx -l /opt/apache-jmeter-5.5/bin/Docker/$curTime_Results_0111.jtl -e -o /opt/apache-jmeter-5.5/bin/Docker/HTML_NEW_Results_3110
+./jmeter.sh -n -t /opt/apache-jmeter-5.5/bin/Docker/SampleTestScript_Google.jmx -l /opt/apache-jmeter-5.5/bin/Docker/Results_0711.jtl -e -o /opt/apache-jmeter-5.5/bin/Docker/HTML_NEW_Results_3110
  
 cd Docker
+
 #Push the code
-git add .
 git config --global user.name "SaiKumar"
 git config --global user.email "saikumar.vodhulu@qentelli.com"
 
