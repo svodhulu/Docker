7723d0ec260f3fac13cf8c1123851338ec4395bd
svodhulu <116167297+svodhulu@users.noreply.github.com>
2022-11-08 11:43:13 +0530

    Update Jmeterrun.sh

--- Jmeterrun.sh
+++ Jmeterrun.sh
@@ -6,13 +6,12 @@ echo "*************Running Jmeterun.sh file**********"
 #git clone https://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@github.com/svodhulu/Docker.git
 
 
-#curTime= $(date+"%m-%d-%y_%H-%M-%S")
-#curTime= $(echo $curTime|sed 's/-//g'|sed 's/_//g')
+curtime=$(date +%d%m%Y%H%M%S)
 
 echo "Current time in epochtime $curTime"
 
 cd ..
-sh jmeter.sh -n -t /opt/apache-jmeter-5.5/bin/Docker/SampleTestScript_Google.jmx -l /opt/apache-jmeter-5.5/bin/Docker/Results_0711_NEW.jtl -e -o /opt/apache-jmeter-5.5/bin/Docker/HTML_NEW_Results_0711
+sh jmeter.sh -n -t /opt/apache-jmeter-5.5/bin/Docker/SampleTestScript_Google.jmx -l /opt/apache-jmeter-5.5/bin/Docker/${curTime}.jtl -e -o /opt/apache-jmeter-5.5/bin/Docker/HTML_${curTime}
  
 cd Docker
 
